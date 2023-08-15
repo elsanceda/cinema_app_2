f092b643c5ac440cf56315b9f91ae138239c40e6
igsanceda <igsanceda@up.edu.ph>
2023-08-15 23:57:07 +0800

    Ensure type fields are not null

--- app/graphql/types/cinema_type.rb
+++ app/graphql/types/cinema_type.rb
@@ -3,9 +3,9 @@
 module Types
   class CinemaType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String
-    field :location, String
-    field :seats, Integer
+    field :name, String, null: false
+    field :location, String, null: false
+    field :seats, Integer, null: false
     # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,9 +3,9 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :full_name, String
-    field :mobile_number, String
-    field :email, String
+    field :full_name, String, null: false
+    field :mobile_number, String, null: false
+    field :email, String, null: false
     # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
