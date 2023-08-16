2d3136c5e5a1e455cc54eabdc0d857baf696aca4
igsanceda <igsanceda@up.edu.ph>
2023-08-16 20:55:54 +0800

    Add showing validations and tests

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -2,4 +2,9 @@ class Showing < ApplicationRecord
   belongs_to :cinema
   belongs_to :movie
   default_scope -> { order(created_at: :desc) }
+  validates :cinema_id, presence: true
+  validates :movie_id, presence: true
+  validates :timeslot, presence: true
+  validates :timeslot, uniqueness: { scope: :cinema,
+                                     message: "for that cinema already taken" }
 end
